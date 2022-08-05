c0672230d2d35712ba36477011d280ae687b2d1d
chicagoist <le.legioner@gmail.com>
2022-08-05 11:03:48 +0200

    Fancier Output with Modules

--- ch02/fancierOutputWithModules.pl
+++ ch02/fancierOutputWithModules.pl
@@ -10,4 +10,28 @@ my $workbook = Spreadsheet::WriteExcel->new('perl.xls');
 # Add a worksheet
 my $worksheet = $workbook->add_worksheet();
 
-$worksheet->write( 'A1', 'Hello Excel!' );
+my $red_background = $workbook->add_format(
+    color    => 'white',
+    bg_color => 'red',
+    bold     => 1,
+);
+my $bold = $workbook->add_format(bold => 1,);
+
+$worksheet->write('A1', 'Hello Excel!');
+$worksheet->write(0, 1, 'Hello Excel'); # in Excel's B1 cell
+
+$worksheet->write( 0, 0, 'Colored cell', $red_background );
+$worksheet->write( 0, 1, 'bold cell', $bold );
+
+my $product_code = '01234';
+$worksheet->write_string( 0, 2, $product_code );
+
+
+$worksheet->write( 'A2', 35 );
+$worksheet->write( 'B2', 42 );
+#$worksheet->write( 'C2', '= A2 + B2');
+$worksheet->write_formula('C2', '= A2 + B2');
+
+# There’s a lot more to this module, and we should be able to quickly figure
+# out its other feature by checking its documentation. We’ll also show some
+# more examples as we go through the chapters on references.
